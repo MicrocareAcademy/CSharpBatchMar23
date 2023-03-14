7bdbfcd868ee48f1effe54af4c24f0b7e4b5a5c1
Sajjad <sajjad@microcare.io>
2023-03-14 10:31:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿
+
+// int num = 0;  only one time initialization
+
+
+// num < 20
+// body will be executed
+// iterator stmt, num ++ 
+
+//Console.WriteLine("Numbers:");
+
+//for(int num = 1; num <= 10; num++)
+//{
+//    // body of for loop
+//    // body will be executed after condition(expression is true)
+
+//    Console.Write($"{num} ");
+//}
+
+//int num = 1;
+//for(;num <= 10;)  //for(int num = 1; num <= 10; num++)
+//{
+//    Console.Write($"{num} ");
+
+//    num++;
+//}
+
+//int x = 0;
+//for(;x < 1;)
+//{
+//    Console.Write($"{x}");
+//}
+
+
+// Summation of first 10 natural numbers
+
+//int sum = 0;
+//int noOfNaturalNumbers = Convert.ToInt32(Console.ReadLine());
+
+//for(int num = 1; num <= noOfNaturalNumbers; num++)
+//{
+//    sum = sum + num;
+//}
+
+//Console.WriteLine($"Summation of first {noOfNaturalNumbers} natural numbers: {sum}");
+
+//// 1+2+3+4+5+6+7+8+9+10 = 
+
+
+// 10 to 1
+
+//for(int num = 10; num >= 1; num--)
+//{
+//    Console.WriteLine($"Number: {num}");
+//}
+
+
+
+//for (int i = 0; i < 10; i++)
+//{
+//    if (i == 5)
+//    {
+//        break;
+//    }
+
+//    Console.WriteLine($"Value of i: {i}");
+//}
+
+
+//for (int i = 0,j=0; i + j < 5; i++, j++)
+//{
+//    Console.WriteLine($"Value of i: {i}, J: {j} ");
+//}
+
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(j + " ");
+    }
+
+    Console.WriteLine();
+
+}
+
+
+/*
+ 1 = 1
+
+ 1 2 = 3
+ 
+ 1 2 3 = 6
+ */
---
+++ Sessions/IfElse_Switch.cs
@@ -0,0 +1,101 @@
+﻿
+
+//Console.WriteLine("choose any color from options(red/green/black/white");
+
+//string color = Console.ReadLine();
+
+//switch (color)
+//{
+//    case "red":
+//        Console.WriteLine("Its red");
+//        break;
+
+//    case "green":
+//        Console.WriteLine("Its green");
+//        break;
+
+//    case "black":
+//        Console.WriteLine("Its black");
+//        break;
+
+//    case "white":
+//        Console.WriteLine("Its white");
+//        break;
+//    default:
+//        Console.WriteLine("No color matches");
+//        break;
+//}
+
+//// One good exmaple of SWITCH with BOOLEAN
+
+
+//Console.WriteLine("Enter any character");
+
+//string vowel = Console.ReadLine().ToLower();
+
+//switch(vowel)
+//{
+//    case "a":
+//    case "e":
+//    case "i":
+//    case "o":
+//    case "u":
+//        Console.WriteLine("Its a vowel");
+//        break;
+//    default:
+//        Console.WriteLine("Its not a vowel");
+//        break;
+
+//}
+
+
+
+//string myclient = Console.ReadLine().ToLower();
+
+
+//switch (myclient)
+//{
+//    case "infosys":
+//        break;
+//    case "tcs":
+//        break;
+//    case "microsoft":
+//        break;
+//    default:
+//        break;
+//}
+
+
+
+//Console.WriteLine("Choose any operator + - * /");
+
+//Console.Write("Enter Operator: ");
+
+//string dummyText = Console.ReadLine();
+
+//Console.WriteLine($"my dummy text: {dummyText}");
+
+
+//bool itsBoy = false;
+
+//string gender = itsBoy ? "Male" : "Female";
+//Console.WriteLine($"Its {gender}");
+
+
+//string myClient = "microcare";
+
+//string msgTxt = myClient == "microcare" ? "My Client"  : "Not my Client";
+
+
+// 
+
+// IF 
+// IF ELSE 
+// IF ELSE IF ELSE 
+// SWITCH 
+// TERNARY OPERATOR 
+
+
+
+
+
