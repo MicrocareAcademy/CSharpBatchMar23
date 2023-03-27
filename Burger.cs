10f0114d42107ae3a23b960b4ecee028fc8f5fe9
Sajjad <sajjad@microcare.io>
2023-03-27 11:16:08 +0530

    Methods ....

---
+++ Burger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBatchMar23
+{
+    public static class Burger
+    {
+        public static void PrepareBreads()
+        {
+            Console.WriteLine("Breads Ready");
+        }
+
+        public static void PrepareCheeseSlice()
+        {
+            Console.WriteLine("Cheese Slice Ready");
+        }
+
+        public static void PrepareVegPatty()
+        {
+            Console.WriteLine("Veg Patty Ready");
+        }
+
+        public static void PrepareChickePatty()
+        {
+            Console.WriteLine("Chicken Patty Ready");
+        }
+
+        public static void PrepareSalad()
+        {
+            Console.WriteLine("Salad Ready");
+        }
+
+    }
+}
---
+++ MyCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBatchMar23
+{
+    public static class MyCalculator
+    {
+        static public int Add(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+
+        static public int Add(int num1, int num2,int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        static public int Add(int num1, int num2, int num3,int num4)
+        {
+            return num1 + num2 + num3 + num4;
+        }
+
+        static public int Sub(int num1,int num2)
+        {
+            return num1 - num2;
+        }
+
+        static public int multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
