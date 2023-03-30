d831b16f11c24f0831d4b875af6d9e422366196e
Sajjad <sajjad@microcare.io>
2023-03-30 11:01:06 +0530

    Properties / Encapsulation

--- Student.cs
+++ Student.cs
@@ -8,11 +8,56 @@ namespace CSharpBatchMar23
 {
     public class Student
     {
-        public string FirstName;
+        private string firstName; // FIELD
 
-        public string LastName;
+        // ANother way of settings and getting data from field using methods
+        public void SetFirstName(string fName)
+        {
+            firstName = fName;
+        }
+        public string GetFirstName()
+        {
+            return firstName;
+        }
 
-        public int Age;
+        public string FirstName  // PROPERTY(this is spokeperson for the FIELD
+        {
+            get
+            {
+                return firstName;  // whenever i am accessing the property, get method will be called
+            }
+            set
+            {
+                firstName = value;  // whenever i am setting the property, set method will be called
+            }
+        }
+
+       
+        public string lastName;
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
 
         public DateTime DOB;
 
