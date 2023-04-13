d4bfda456fbd67ea6af1c03325675b3fe2bc16ef
Sajjad <sajjad@microcare.io>
2023-04-13 11:32:55 +0530

    LIST OF OBJECTS

---
+++ LINQ/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBatchMar23.LINQ
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set;}
+        public string MobileNo { get; set; }
+        public string City { get; set;}
+
+        public Person(int id, string name,int age,string mobileNo,string city)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            MobileNo = mobileNo;
+            City = city;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,48 @@
 ﻿
+using CSharpBatchMar23.LINQ;
+
+IList<int> ages = new List<int>();
+ages.Add(10);
+
+IList<int> userIds = new List<int>() 
+                                { 1, 2, 3, 4, 5, 6 };
 
 
-//using CSharpBatchMar23.Polymorphism;
 
-//Drawing drawing = new Square();
+foreach (var age in ages)
+{
 
-//drawing.Area();
-
-
-
-CSharpBatchMar23.Students.UserInfo studentUser = new CSharpBatchMar23.Students.UserInfo();
-studentUser.StudentUserName = "asdasd";
+}
 
 
-CSharpBatchMar23.UserInfo.UserInfo user = new CSharpBatchMar23.UserInfo.UserInfo();
-user.MobileNoUSA = "q123123";
+//IList<Person> persons = new List<Person>();
 
+//var p1 = new Person(1, "Sajjad", 34, "123-123-1231", "Kurnool");
+//persons.Add(p1);
+
+//var p2 = new Person(1, "Srikant", 25, "10023-00-1231", "Kurnool");
+//persons.Add(p2);
+
+//var p3 = new Person(1, "Praveen", 24, "741-741-741", "Kurnool");
+//persons.Add(p3);
+
+IList<Person> persons = new List<Person>()
+{
+    new Person(1, "Sajjad", 34, "123-123-1231", "Kurnool"),
+    new Person(2, "Srikant", 25, "10023-00-1231", "Kurnool"),
+    new Person(3, "Praveen", 24, "741-741-741", "Kurnool")
+};
+
+persons.Add(new Person(4, "Hammad", 26, "8888-741-741", "Kurnool"));
+
+var srikantPer = persons.FirstOrDefault(p => p.Id == 2);
+
+srikantPer.Name = "SAI Srikant";
+
+foreach (Person p in persons)
+{
+    
+    Console.WriteLine($"Id: {p.Id}, Name: {p.Name}, City: {p.City}");
+}
 
 
