ac079ff946e06edc1c2b0bcfc274598114be7a8a
Sajjad <sajjad@microcare.io>
2023-04-14 11:07:49 +0530

    LINQ/ Query,Method Expressions

--- Program.cs
+++ Program.cs
@@ -1,48 +1,53 @@
-﻿
-using CSharpBatchMar23.LINQ;
-
-IList<int> ages = new List<int>();
-ages.Add(10);
-
-IList<int> userIds = new List<int>() 
-                                { 1, 2, 3, 4, 5, 6 };
-
-
-
-foreach (var age in ages)
-{
-
-}
-
-
-//IList<Person> persons = new List<Person>();
-
-//var p1 = new Person(1, "Sajjad", 34, "123-123-1231", "Kurnool");
-//persons.Add(p1);
-
-//var p2 = new Person(1, "Srikant", 25, "10023-00-1231", "Kurnool");
-//persons.Add(p2);
-
-//var p3 = new Person(1, "Praveen", 24, "741-741-741", "Kurnool");
-//persons.Add(p3);
+﻿using CSharpBatchMar23.LINQ;
+using System.Xml.Linq;
 
 IList<Person> persons = new List<Person>()
 {
     new Person(1, "Sajjad", 34, "123-123-1231", "Kurnool"),
-    new Person(2, "Srikant", 25, "10023-00-1231", "Kurnool"),
-    new Person(3, "Praveen", 24, "741-741-741", "Kurnool")
+    new Person(2, "Srikant", 25, "10023-00-1231", "Nandyal"),
+    new Person(3, "Praveen", 24, "741-741-741", "Atmakur"),
+    new Person(4, "Sushma", 24, "741-525-741", "Atmakur")
 };
 
-persons.Add(new Person(4, "Hammad", 26, "8888-741-741", "Kurnool"));
+persons.Add(new Person(5, "Hammad", 26, "8888-741-741", "Hyderabad"));
+persons.Add(new Person(5, "Sreenivasulu", 26, "999-741-741", "Kurnool"));
+persons.Add(new Person(6, "Kalyani", 26, "9999-741-741", "Bangalore"));
 
-var srikantPer = persons.FirstOrDefault(p => p.Id == 2);
+//var firstPersonOfKnl = persons.Where(p => p.City == "Kurnool").First();
 
-srikantPer.Name = "SAI Srikant";
+////var firstPersonOfKnl = persons.Where(p => p.City == "Newyork").FirstOrDefault();
 
-foreach (Person p in persons)
+
+//Console.WriteLine("First Person Of KNL");
+//Console.WriteLine($"Name: {firstPersonOfKnl.Name} / ID: {firstPersonOfKnl.Id} / Mobile No: {firstPersonOfKnl.MobileNo}");
+
+//var lastPersonOfKnl = persons.Where(p => p.City == "Kurnool").Last();
+//Console.WriteLine("Last Person Of KNL");
+//Console.WriteLine($"Name: {lastPersonOfKnl.Name} / ID: {lastPersonOfKnl.Id} / Mobile No: {lastPersonOfKnl.MobileNo}");
+
+
+// I want to get only Names from the collection
+
+//var names = (from p in persons
+//             where p.City == "Kurnool"
+//             select p.Name);
+
+
+//var names2 = persons.Select(person => person.Name).ToList();
+
+//foreach(var name in names2)
+//{
+//    Console.WriteLine(name);
+//}
+
+
+
+var personsWithBasicData = persons.Select(x => new 
+                                                    { PersonID = x.Id,
+                                                      PersonName = x.Name });
+
+foreach(var person in personsWithBasicData)
 {
-    
-    Console.WriteLine($"Id: {p.Id}, Name: {p.Name}, City: {p.City}");
+    Console.WriteLine($"PersonID : {person.PersonID} , Name: {person.PersonName}");
 }
-
 
